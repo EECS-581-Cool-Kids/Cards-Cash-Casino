2508ddfb836a59b9d84124af6e86a9c6d1940d8e
modmo22 <modcox2@gmail.com>
2024-11-04 01:46:59 -0600

    Added PokerHand.cs

---
+++ CardsCashCasino/Data/PokerHand.cs
@@ -0,0 +1,25 @@
+﻿/*
+ *  Module Name: PokerHand.cs
+ *  Purpose: This module models the AI hands in the poker games.
+ *  Inputs: None
+ *  Outputs: None
+ *  Additional code sources: None
+ *  Developers: Mo Morgan
+ *  Date: 11/32024
+ *  Last Modified: 11/3/2024
+ *  Preconditions: None
+ *  Postconditions: None
+ *  Error/Exception conditions: None
+ *  Side effects: None
+ *  Invariants: None
+ *  Known Faults: None encountered
+ */
+
+namespace CardsCashCasino.Data;
+
+/// <summary>
+/// The <c>PokerHand</c> class represents the AI opponents' poker hands.
+/// </summary>
+public class PokerHand : CardHand
+{
+}
