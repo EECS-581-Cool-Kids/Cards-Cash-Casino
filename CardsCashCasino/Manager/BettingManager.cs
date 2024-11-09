f3705d7766c06c1a69f3ee52a45c8f8423e60b6a
Jacob Wilkus <jwilkus1@gmail.com>
2024-11-09 14:03:05 -0600

    cleanup

--- CardsCashCasino/Manager/BettingManager.cs
+++ CardsCashCasino/Manager/BettingManager.cs
@@ -68,43 +68,6 @@ namespace CardsCashCasino.Manager
 
     public class CashValueIndicator
     {
-        internal class CashValueDigit
-        {
-            /// <summary>
-            /// Texture for the second digit.
-            /// </summary>
-            private Texture2D? _digitTexture;
-
-            /// <summary>
-            /// Rectnagle for the first digit.
-            /// </summary>
-            private Rectangle? _digitRectangle;
-
-            /// <summary>
-            /// Sets the position at a given x and y coordinate.
-            /// </summary>
-            public void SetPosition(int xPos, int yPos)
-            {
-                _digitRectangle = new Rectangle(xPos, yPos, 21, 24);
-            }
-
-            /// <summary>
-            /// Updates the digit value.
-            /// </summary>
-            /// <param name="value"></param>
-            public void Update(int value)
-            {
-                _digitTexture = DisplayIndicatorUtil.GetDigitTexture(value);
-            }
-
-            /// <summary>
-            /// Draw method for the value indicator.
-            /// </summary>
-            public void Draw(SpriteBatch spriteBatch)
-            {
-                if (_digitRectangle is not null)
-                    spriteBatch.Draw(_digitTexture, (Rectangle)_digitRectangle, Color.White);
-            }
-        }
+        public List<IndicatorDigit> digits = new();
     }
 }
